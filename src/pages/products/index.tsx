d09e2bd58cf8c91466937dc69ff94552d7802627
MaJunghan <wjdgksak@naver.com>
2022-06-29 18:28:51 +0900

    scss style 적용

--- src/pages/products/index.tsx
+++ src/pages/products/index.tsx
@@ -16,7 +16,6 @@ const ProductListPage = () => {
       <li key={product.id} className="product-item">
         <p className="product-item-cetegory">{product.category}</p>
         <p className="product-item-title">{product.title}</p>
-        <p className="product-item-description">{product.description}</p>
         <img className="product-item-image" src={product.image} />
         <span className="product-item-price">{product.price}</span>
         <span className="product-item-rate">{product.rating.rate}</span>
