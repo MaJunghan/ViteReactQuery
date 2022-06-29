a7ef1372cc1a12e968bc6b3f1bb125c2a3750ed1
MaJunghan <wjdgksak@naver.com>
2022-06-29 15:15:23 +0900

    yarn react-query provider 셋팅

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,19 @@
+import { QueryClientProvider } from "react-query";
 import { useRoutes } from "react-router-dom";
+import { getClient } from "./queryClient";
 import { routes } from "./routes"; // or use Vite's alias to simplify import path for nested components
+import { ReactQueryDevtools } from "react-query/devtools";
 
 function App() {
   const element = useRoutes(routes);
-  return element;
+  const queryClient = getClient();
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      {element}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
 }
 
 export default App;
